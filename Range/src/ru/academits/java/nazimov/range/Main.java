acaf33630edb873035457c83a021971ee45faae0
Aleksandr <50_Sanya@mail.ru>
2020-10-15 14:46:04 +0700

    Правка стиля написания кода.

--- Range/src/ru/academits/java/nazimov/range/Main.java
+++ Range/src/ru/academits/java/nazimov/range/Main.java
@@ -25,25 +25,28 @@ public class Main {
         System.out.println();
 
         Range range2 = new Range(6, 9);
-
         Range[] range3 = range1.getIntervalsConcatenation(range2);
+
         for (Range e : range3) {
             e.print();
         }
-        System.out.println();
 
+        System.out.println();
         Range range4 = range2.getIntervalsIntersection(range1);
+
         if (range4 != null) {
             range4.print();
         } else {
             System.out.println("Range4 = null");
         }
-        System.out.println();
 
+        System.out.println();
         Range[] range5 = range1.getIntervalDifference(range2);
+
         for (Range e : range5) {
             e.print();
         }
+
         System.out.println();
     }
 }
