19d4521b4e27e823bd95c137b005effcbb187e41
Aleksandr <50_Sanya@mail.ru>
2020-11-11 17:11:38 +0700

    Правка класса Main. (корректирование вывода информации о фигурах) Исправление ошибок.

--- Shapes/src/ru/academits/java/nazimov/shape_main/Main.java
+++ Shapes/src/ru/academits/java/nazimov/shape_main/Main.java
@@ -3,7 +3,6 @@ package ru.academits.java.nazimov.shape_main;
 import ru.academits.java.nazimov.shape.*;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,36 +18,26 @@ public class Main {
         shapes[7] = new Circle(4);
 
         Shape maxArea = getAreaMax(shapes);
-        System.out.printf("Shape: %s%n Width: %.2f%n Height: %.2f%n Area: %.2f%n Perimeter: %.2f%n Area: %.2f%n",
-                maxArea.getClass(), maxArea.getWidth(), maxArea.getHeight(),
-                maxArea.getArea(), maxArea.getPerimeter(), maxArea.getArea());
+        System.out.printf("Shape: %s%n Width: %.2f%n Height: %.2f%n Area: %.2f%n Perimeter: %.2f%n",
+                maxArea.toString(), maxArea.getWidth(), maxArea.getHeight(),
+                maxArea.getArea(), maxArea.getPerimeter());
 
         System.out.println();
 
         Shape secondPerimeter = getSecondLargestPerimeter(shapes);
-        System.out.printf("Shape: %s%n Width: %.2f%n Height: %.2f%n Area: %.2f%n Perimeter: %.2f%n Area: %.2f%n",
-                secondPerimeter.getClass(), secondPerimeter.getWidth(), secondPerimeter.getHeight(),
-                secondPerimeter.getArea(), secondPerimeter.getPerimeter(), secondPerimeter.getArea());
+        System.out.printf("Shape: %s%n Width: %.2f%n Height: %.2f%n Area: %.2f%n Perimeter: %.2f%n",
+                secondPerimeter.toString(), secondPerimeter.getWidth(), secondPerimeter.getHeight(),
+                secondPerimeter.getArea(), secondPerimeter.getPerimeter());
     }
 
     public static Shape getAreaMax(Shape[] shapes) {
-        Arrays.sort(shapes, new Comparator<Shape>() {
-            @Override
-            public int compare(Shape o1, Shape o2) {
-                return (int) (o2.getArea() - o1.getArea());
-            }
-        });
+        Arrays.sort(shapes, (o1, o2) -> (int) (o2.getArea() - o1.getArea()));
 
         return shapes[0];
     }
 
     public static Shape getSecondLargestPerimeter(Shape[] shapes) {
-        Arrays.sort(shapes, new Comparator<Shape>() {
-            @Override
-            public int compare(Shape o1, Shape o2) {
-                return (int) (o2.getPerimeter() - o1.getPerimeter());
-            }
-        });
+        Arrays.sort(shapes, (o1, o2) -> (int) (o2.getPerimeter() - o1.getPerimeter()));
 
         return shapes[1];
     }
