4269d1de895d9701ead98e88c314e565f5f6b140
Aleksandr <50_Sanya@mail.ru>
2020-11-20 14:32:59 +0700

    Исправление ошибок. Оформил три отдельных функции.

--- ArrayListHome/src/ru/academits/nazimov/arraylisthome/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/nazimov/arraylisthome/ArrayListHome.java
@@ -3,38 +3,65 @@ package ru.academits.nazimov.arraylisthome;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayListHome {
     public static void main(String[] args) {
         // Задача 1
-        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<String> lines = new ArrayList<>();
 
-        try (Scanner scanner = new Scanner(new FileReader("ArrayListHome/input.txt"))) {
-            while (scanner.hasNext()) {
-                numbers.add(scanner.nextInt());
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("ArrayList (read from file) " + numbers);
+        readFileIntoList(lines);
+        System.out.println("ArrayList (read from file) " + lines);
 
         // Задача 2
-        numbers.removeIf(integer -> (integer % 2 == 0));
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 3, 1, 7, 5, 6, 6, 7));
+
+        removeEvenNumbers(numbers);
         System.out.println("ArrayList (remove even numbers) " + numbers);
 
-        // Задача 3 - если для это кода есть готовое решение, подскажите какое (я искал, но найти не получилось)
-        ArrayList<Integer> uniqueNumbers = new ArrayList<>();
+        // Задача 3
+        ArrayList<Integer> uniqueNumbers = removeDuplicates(numbers);
 
-        for (int i = 0; i < numbers.size(); i++) {
-            int lastIndexOf = numbers.lastIndexOf(numbers.get(i));
+        System.out.println("ArrayList (unique lines) " + uniqueNumbers);
+    }
 
-            if (i == lastIndexOf) {
-                uniqueNumbers.add(numbers.get(i));
+    public static void readFileIntoList(ArrayList<String> arrayList) {
+        try (Scanner scanner = new Scanner(new FileReader("ArrayListHome/input.txt"))) {
+            while (scanner.hasNext()) {
+                arrayList.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        }
+    }
+
+    public static void removeEvenNumbers(ArrayList<Integer> arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i) % 2 == 0) {
+                arrayList.remove(i);
+                i--;
             }
         }
+    }
 
-        System.out.println("ArrayList (unique numbers) " + uniqueNumbers);
+    public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> arrayList) {
+        ArrayList<Integer> uniqueNumbers = new ArrayList<>();
+        uniqueNumbers.ensureCapacity(arrayList.size());
+
+        uniqueNumbers.add(arrayList.get(0));
+
+        duplicate:
+        for (Integer i : arrayList) {
+            for (Integer j : uniqueNumbers) {
+                if (i.equals(j)) {
+                    continue duplicate;
+                }
+            }
+
+            uniqueNumbers.add(i);
+        }
+
+        return uniqueNumbers;
     }
 }
